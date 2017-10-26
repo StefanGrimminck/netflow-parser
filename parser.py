23201e817987dabe62d7ea5189852165302f7021
Stefan Grimminck <s.j.grimminck@gmail.com>
2017-10-26 15:30:02 +0200

    fixed formatting in output file

--- parser.py
+++ parser.py
@@ -30,16 +30,16 @@ def cleanCSV(IPRANGETREE, OUTFILE):
 
         reader = {}
         with open(IPRANGETREE.name, 'r') as f:
-            reader =f.read()
+            reader = f.read()
             ranges = ast.literal_eval(reader)
 
             for row in csv.reader(inp, delimiter=','):
-                ip = row[0].strip()
+                row[0] = row[0].strip()
 
-                portnr = row[1].strip()
+                row[1] = row[1].strip()
                 # check if portnumber is lower than 1024
-                if 0 < float(portnr) <= 1024:
-                    a, b, c, d = ip.split('.')
+                if 0 < float(row[1]) <= 1024:
+                    a, b, c, d = row[0].split('.')
 
                     if ownerChecker(ranges, a, b, c):
                         writer.writerow(row)
