422077d2101554618621473b333d5f501b75b2a6
Stefan Grimminck <s.j.grimminck@gmail.com>
2017-11-01 16:34:16 +0100

    Added argument parsing to dataformatter and formatted some code.

--- dataformatter.py
+++ dataformatter.py
@@ -1,22 +1,49 @@
 #!/usr/bin/env python
 import csv
 import sys
+import argparse
 
 
-PORTLIST = [102,  110,  143,  1911,  20000,  21,  22,  23,  2323,  25,  443,  445,  465,  502,  7547,80 , 8080,  8888,  993,  995]
+def sortOnPorts(INFILE):
+    PORTLIST = [102,  110,  143,  1911,  20000,  21,  22,  23,  2323,  25,  443,  445,  465,  502,  7547,80 , 8080,  8888,  993,  995]
+    foundPorts = []
 
-filename = 'FileName'
-with open(filename, newline='') as f:
-    reader = csv.reader(f)
-    try:
-        for row in reader:
-            row[0] = row[0].strip()
-            row[1] = row[1].strip()
+    filename = INFILE.name
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        try:
+            for row in reader:
+                row[0] = row[0].strip()
+                row[1] = row[1].strip()
+                portnr = float(row[1]) #float because of the outputfile containing 0.0 as a port
 
-            if float(row[1])in PORTLIST:
-                with open(row[1] + '.csv', 'a', newline='') as csvfile:
-                    writer = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                    writer.writerow([row[0]])
-    except:
-        pass
+                if portnr in PORTLIST:
+
+                    if portnr not in foundPorts:
+                        foundPorts.append(int(portnr))
+
+                    with open(row[1] + '.csv', 'a', newline='') as csvfile:
+                        writer = csv.writer(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                        writer.writerow([row[0]])
+        except:
+            pass
+    return foundPorts
+
+
+
+def main(arguments):
+    parser = argparse.ArgumentParser( description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('infile',                  help="Input file",                  type=argparse.FileType('r'))
+
+
+    args = parser.parse_args(arguments)
+    INFILE = args.infile
+    portlist = sortOnPorts(INFILE)
+
+    portlist.sort()
+    print(portlist)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
--- parser.py
+++ parser.py
@@ -10,11 +10,11 @@ import ipaddress
 def nfdumpToCSV(IPRANGETREE, INFILE, OUTFILE, IPvTYPE, IPV6RNG):
     if (IPvTYPE == '6'):
         command = 'nfdump -6 -r ' + INFILE.name + \
-            ' -q -o "fmt:%da,%dp" > ' + OUTFILE.name + '.RAW'
+                  ' -q -o "fmt:%da,%dp" > ' + OUTFILE.name + '.RAW'
         os.system(command)
     elif (IPvTYPE == '4'):
         command = 'nfdump -r  ' + INFILE.name + \
-            ' -q -o "fmt:%da,%dp" > ' + OUTFILE.name + '.RAW'
+                  ' -q -o "fmt:%da,%dp" > ' + OUTFILE.name + '.RAW'
         os.system(command)
 
     cleanCSV(IPRANGETREE, OUTFILE, IPvTYPE, IPV6RNG)
@@ -28,29 +28,31 @@ def ownerChecker(ranges, block1, block2, block3):
         return False
 
 
-
 def cleanCSV(IPRANGETREE, OUTFILE, IPvTYPE, IPV6RNG):
     with open(OUTFILE.name + '.RAW', 'r') as inp, open(OUTFILE.name, 'w') as out:
         writer = csv.writer(out)
 
         if (IPvTYPE == '4'):
+
+            rows = [] #list used to check for duplicates
             with open(IPRANGETREE.name, 'r') as f:
                 reader = f.read()
                 ranges = ast.literal_eval(reader)
 
                 for row in csv.reader(inp, delimiter=','):
-                    row[0] = row[0].strip()
-                    row[1] = row[1].strip()
+                    ip = row[0].strip()
+
+                    portnr = row[1].strip()
                     # check if portnumber is lower than 1024
-                    if 0 < float(row[1]) <= 1024:
-                        a, b, c, d = row[0].split('.')
+                    if 0 < float(portnr) <= 1024:
+                        a, b, c, d = ip.split('.')
 
                         if ownerChecker(ranges, a, b, c):
                             if row not in rows:
                                 rows.append(row)
                                 writer.writerow(row)
 
-        elif(IPvTYPE == '6'):
+        elif (IPvTYPE == '6'):
             for row in csv.reader(inp, delimiter=','):
                 row[0] = row[0].strip()
                 row[1] = row[1].strip()
@@ -59,27 +61,20 @@ def cleanCSV(IPRANGETREE, OUTFILE, IPvTYPE, IPV6RNG):
                     if ipaddress.ip_address(row[0]) in ipaddress.ip_network(IPV6RNG):
                         writer.writerow(row)
 
-
-
     os.remove(OUTFILE.name + '.RAW')
 
 
 def main(arguments):
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file",
-                        type=argparse.FileType('r'))
-    parser.add_argument('outfile', help="Output file",
-                        type=argparse.FileType('w'))
-    parser.add_argument('--rangetree', help="Range tree with addresses",
-                        type=argparse.FileType('r'))
-    parser.add_argument('--ipv6range', help="IPv6 range")
-    parser.add_argument(
-        'ipv', choices=['4', '6'], help='for IPv6 "6" for IPv4 "4"')
+    parser = argparse.ArgumentParser( description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('infile',                  help="Input file",                  type=argparse.FileType('r'))
+    parser.add_argument('outfile',                 help="Output file",                 type=argparse.FileType('w'))
+    parser.add_argument('--rangetree',             help="Range tree with addresses",   type=argparse.FileType('r'))
+    parser.add_argument('--ipv6range',             help="IPv6 range",                  type=str)
+    parser.add_argument('ipv', choices=['4', '6'], help='for IPv6 "6" for IPv4 "4"')
 
     args = parser.parse_args(arguments)
     INFILE = args.infile
+
     OUTFILE = args.outfile
     IPvTYPE = args.ipv
     IPRANGETREE = args.rangetree
