e49eca86de10865a2204e72c57522e6264da0baf
jehama <jehama@users.noreply.github.com>
2017-12-01 10:13:17 +0100

    first cleanup of parser.py. Not yet finished.

--- parser.py
+++ parser.py
@@ -5,55 +5,82 @@
 
 #!/usr/bin/env python3
 
-"""A script which uses nfdump to obtain IPadresses and ports.
-These are then filtered using a tree with IPadresses to check if it falls in a desired range.
-The IPadresses are then written to a file."""
+"""A script loads a text file containing IPaddresses and ports.
+These are then filtered using a tree with IPaddresses to check if it falls in a desired range.
+The IPaddresses and ports are then written to a file."""
+
+# Imports
+import argparse
 import ast
 import csv
-import sys
-import argparse
 import ipaddress
+import sys
 
 
-# Function to check if an IPv4 address is found inside the rangetree
-def ownerchecker(ranges, block1, block2, block3):
+def ownerchecker(ranges, block1, block2, block3, block4):
+    """Function to check if an IPV4 address is found inside the rangetree
+
+    Note:
+        This method is only called for IPv4 addresses.
+
+    Args:
+        ranges (Dictionary): A dictionary containing a dictionary containing an array.
+            This "Tree" contains IPv4 addresses that should be saved
+        block1 (int): The first block of a IPv4 address.
+        block2 (int): The second block of a IPv4 address.
+        block3 (int): The third block of a IPv4 address.
+        block4 (int): The fourth block of a IPv4 address.
+
+     Returns:
+         True if it should be saved, False otherwise."""
+    # This is only used to comply with the Pylint standard, which doesn't allow unused variables.
+    block4 = block4
     try:
+        # If you don't own all the IPv4 addresses in the fourth block you can update the following line of code to:
+        # if block 4 in ranges[block1][block2][block3]:
         if block3 in ranges[block1][block2]:
             return True
-    except:
+    # The KeyError is raised if the IPv4 address isn't found in the tree.
+    except KeyError:
         return False
 
 
 def cleancsv4(rangetree, inputfile, outputfile):
-    with open(inputfile, 'r') as inp, open(outputfile, 'w') as out, open(rangetree, 'r') as f:
+    """Assess for every IPv4 address if it should be saved, and if so write it to a file.
+
+    Args:
+        rangetree (Dictionary): A dictionary containing a dictionary containing an array.
+            This 'Tree' contains IPv4 addresses that should be saved
+        inputfile (file): An input file containing the IPv4 addresses.
+        outputfile (file): The file to place the final data in."""
+    with open(inputfile, "r") as inp, open(outputfile, "w") as out, open(rangetree, "r") as tree:
         writer = csv.writer(out)
-        reader = f.read()
+        reader = tree.read()
         ranges = ast.literal_eval(reader)
 
-        for row in csv.reader(inp, delimiter=','):
+        for row in csv.reader(inp, delimiter=","):
             row[0] = row[0].strip()
             row[1] = row[1].strip()
 
             try:
                 if 0 < float(row[1]) <= 1024:
-                    a, b, c, d = row[0].split('.')
+                    block1, block2, block3, block4 = row[0].split(".")
 
-                    if ownerchecker(ranges, a, b, c):
+                    if ownerchecker(ranges, block1, block2, block3, block4):
                         rows = []
 
                         if row not in rows:
                             rows.append(row)
                             writer.writerow(row)
-            except:
+            except ValueError:
                 pass
 
 
-
 def cleancsv6(inputfile, outputfile, iprange):
-    with open(inputfile, 'r') as inp, open(outputfile, 'w') as out:
+    with open(inputfile, "r") as inp, open(outputfile, "w") as out:
         writer = csv.writer(out)
 
-        for row in csv.reader(inp, delimiter=','):
+        for row in csv.reader(inp, delimiter=","):
             row[0] = row[0].strip()
             row[1] = row[1].strip()
             # check if portnumber is lower than 1024
@@ -65,11 +92,11 @@ def cleancsv6(inputfile, outputfile, iprange):
 def main(arguments):
     # Specify required arguments
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file", type=argparse.FileType('r'))
-    parser.add_argument('outfile', help="Output file", type=argparse.FileType('w'))
-    parser.add_argument('--rangetree', help="Range tree with addresses", type=argparse.FileType('r'))
-    parser.add_argument('--ipv6range', help="IPv6 range", type=str)
-    parser.add_argument('ipv', choices=['4', '6'], help='for IPv6 "6" for IPv4 "4"')
+    parser.add_argument("infile", help="Input file", type=argparse.FileType("r"))
+    parser.add_argument("outfile", help="Output file", type=argparse.FileType("w"))
+    parser.add_argument("--rangetree", help="Range tree with addresses", type=argparse.FileType("r"))
+    parser.add_argument("--ipv6range", help="IPv6 range", type=str)
+    parser.add_argument("ipv", choices=["4", "6"], help="for IPv6 '6' for IPv4 '4'")
 
     # Convert args to usable variables
     args = parser.parse_args(arguments)
@@ -80,11 +107,11 @@ def main(arguments):
     ipv6range = args.ipv6range
 
     # Check if all the correct parameters have been given
-    if ipvtype == '4' and infile is not None and outfile is not None and ipv4rangetree is not None:
+    if ipvtype == "4" and infile is not None and outfile is not None and ipv4rangetree is not None:
         cleancsv4(ipv4rangetree.name, infile.name, outfile.name)
-    elif ipvtype == '6' and infile is not None and outfile is not None and ipv6range is not None:
+    elif ipvtype == "6" and infile is not None and outfile is not None and ipv6range is not None:
         cleancsv6(infile.name, outfile.name, ipv6range)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
